9cdb5d27586e82e03c2714f5ce3f173241aaf2c3
mkaur <mandip.kaur@nagarro.com>
2018-01-22 12:57:27 +0530

    first assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+ // newComment='test';
+  comments = [];
+  commnetObj = null;
+
+  constructor() {
+    //this.newComment = null;
+    //this.comments = new Array();
+    this.commnetObj = {
+      comment :null,
+      time : null
+    };
+  }
+
+
+  addComment(value) :void{
+    let newComment = {
+      comment :null,
+      time : null
+    };
+    newComment.comment = value;
+    newComment.time = new Date();
+    this.comments.push(newComment);
+  }
+  
+  
 }
